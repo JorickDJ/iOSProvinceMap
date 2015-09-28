fa2d6132bc8a4d390dcfb9643d707b9a7edc596c
Jorick de Jong <Jorick@Joricks-MBP.lan>
2015-09-28 13:13:49 +0200

    init commit

---
+++ iOSGeoProvinceApp/JsonParser.swift
@@ -0,0 +1,15 @@
+//
+//  JsonParser.swift
+//  iOSGeoProvinceApp
+//
+//  Created by Jorick de Jong on 28/09/15.
+//  Copyright © 2015 Jorick de Jong. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+
+class JsonParser {
+    
+}
---
+++ iOSGeoProvinceApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  iOSGeoProvinceApp
+//
+//  Created by Jorick de Jong on 28/09/15.
+//  Copyright © 2015 Jorick de Jong. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
